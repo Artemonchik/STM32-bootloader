b9f5db6221d95e06c68d13cdc19df9e39516b5f3
p.vasilev <p.vasilev@g.nsu.ru>
2021-04-20 22:37:36 +0700

    Add bootloader file format library (BootloaderFileFormat) Add bootloader file generator console app (BootloaderFileGenerator) (no encryption yet) Update .gitignore

---
+++ BootloaderFileFormat/BootloaderFileFormat/BootloaderFileFormat.cs
@@ -0,0 +1,108 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace BootloaderFileFormat
+{
+    public class BootloaderFile
+    {
+        private static readonly byte[] HeaderBytes = { (byte)'H', (byte)'E', (byte)'A', (byte)'D', (byte)'E', (byte)'R' };
+        private static readonly byte[] DataBytes = {(byte) 'D', (byte) 'A', (byte) 'T', (byte) 'A'};
+        
+        private string _manufacturerName;
+        public string ManufacturerName
+        {
+            set
+            {
+                _manufacturerName = value;
+                _manufacturerNameSize = (short) value.Length;
+            }
+            get => _manufacturerName;
+        }
+
+        private byte[] _data;
+        public byte[] Data
+        {
+            set
+            {
+                _data = value;
+                _size = value.Length;
+            }
+            get => _data;
+        }
+
+        private short _manufacturerNameSize;
+        private int _size { set; get; }
+        private long _unixCreationTime { set; get; }
+        public ushort[] FirmwareVersion { set; get; }
+
+        public BootloaderFile()
+        {
+            FirmwareVersion = new ushort[4];
+            _unixCreationTime = DateTimeOffset.Now.ToUnixTimeSeconds();
+        }
+        
+        public BootloaderFile(string filepath)
+        {
+            var fileStream = new FileStream(filepath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            var binaryReader = new BinaryReader(fileStream, Encoding.ASCII);
+            
+            if (binaryReader.ReadBytes(10) != HeaderBytes)
+            {
+                throw new FormatException();
+            }
+
+            _manufacturerNameSize = binaryReader.ReadInt16();
+            ManufacturerName = new string(binaryReader.ReadChars(_manufacturerNameSize));
+            
+            FirmwareVersion = new ushort[4];
+            for (var i = 0; i < 4; i++)
+            {
+                FirmwareVersion[i] = binaryReader.ReadUInt16();    
+            }
+            
+            _unixCreationTime = binaryReader.ReadInt64();
+
+            if (binaryReader.ReadBytes(4) != DataBytes)
+            {
+                throw new FormatException();
+            }
+            
+            _size = binaryReader.ReadInt32();
+            Data = binaryReader.ReadBytes(_size);
+            binaryReader.Close();
+        }
+
+        public void WriteBootloaderFile(string filepath)
+        {
+            var fileStream = new FileStream(filepath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Write);
+            var binaryWriter = new BinaryWriter(fileStream, Encoding.ASCII);
+            binaryWriter.Write(HeaderBytes);
+            binaryWriter.Write(_manufacturerNameSize);
+            binaryWriter.Write(ManufacturerName.ToCharArray());
+            for (var i = 0; i < 4; i++)
+            {
+                binaryWriter.Write(FirmwareVersion[i]);    
+            }
+            binaryWriter.Write(_unixCreationTime);
+            binaryWriter.Write(DataBytes);
+            binaryWriter.Write(_size);
+            binaryWriter.Write(Data);
+            binaryWriter.Close();
+        }
+
+        public string ToFancyString()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("Manufacturer: ");
+            stringBuilder.AppendLine(ManufacturerName);
+            stringBuilder.Append("Creation time: ");
+            stringBuilder.AppendLine(DateTimeOffset.FromUnixTimeSeconds(_unixCreationTime).ToString());
+            stringBuilder.Append("Firmware size: ");
+            stringBuilder.Append(_size);
+            stringBuilder.AppendLine("B");
+            return stringBuilder.ToString();
+        }
+
+    }
+}
---
+++ BootloaderFileGenerator/BootloaderFileGenerator/BootloaderFileGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using BootloaderFileFormat;
+
+namespace BootloaderFileGenerator
+{
+    class Program
+    {
+        //args[0] - input binary path
+        //args[1] - manufacturer name
+        //args[2] - firmware version (e.g. 1.3.3.7)
+        //args[3] - output path
+        static void Main(string[] args)
+        {
+            var binaryReader =
+                new BinaryReader(new FileStream(args[0], FileMode.Open, FileAccess.Read, FileShare.Read));
+            
+            var rawFirmwareBinaryList = new List<byte>();
+            while (true)
+            {
+                byte tempByte;
+                try
+                {
+                    tempByte = binaryReader.ReadByte();
+                }
+                catch (EndOfStreamException)
+                {
+                    break;
+                }
+
+                rawFirmwareBinaryList.Add(tempByte);
+            }
+
+            var encryptedBinary =  Encrypt(rawFirmwareBinaryList.ToArray());
+
+            var file = new BootloaderFile {Data = encryptedBinary, ManufacturerName = args[1]};
+            var splitVersion = args[2].Split(".");
+            for (var i = 0; i < 4; i++)
+            {
+                file.FirmwareVersion[i] = ushort.Parse(splitVersion[i]);
+            }
+            file.WriteBootloaderFile(args[3]);
+            Console.WriteLine(file.ToFancyString());
+        }
+
+        //TBD
+        private static byte[] Encrypt(byte[] arr)
+        {
+           return arr;
+        }
+    }
+    
+}
