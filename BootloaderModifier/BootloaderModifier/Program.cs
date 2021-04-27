e607411a8473ea7bf393311450c1d3d420871ee4
p.vasilev <p.vasilev@g.nsu.ru>
2021-04-27 11:07:29 +0700

    Add BootloaderModifier

---
+++ BootloaderModifier/BootloaderModifier/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using CommandLine;
+
+namespace BootloaderModifier
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<ModifierOptions>(args)
+                .WithParsed<ModifierOptions>(opts =>
+                    {
+                        var genKey = opts.GenKey;
+                        var writer = new BinaryWriter(new FileStream(genKey, FileMode.OpenOrCreate, FileAccess.Write));
+                        var key = new byte[32];
+                        RandomNumberGenerator.Create().GetBytes(key);
+                        writer.Write(key);
+                        writer.Close();
+                    }
+                );
+        }
+    }
+
+    internal class ModifierOptions
+    {
+        [Option('k', "generate_key", Required = false, HelpText = "Generate a key.", Default = "key.bin")]
+        public string GenKey { set; get; }
+    }
+}
